36eb95dce1a7faa740c8143dfb47d66583f44234
Nguyen Hai <sunsnow0049@gmail.com>
2026-03-27 13:50:45 +0700

    Delete Intern_roadmap directory

--- Intern_roadmap/Xác định enum, trạng thái Task_w1/TaskStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.BKENDelitesport.Service;
-
-public enum TaskStatus {
-    TODO,
-    IN_PROGRESS,
-    DONE
-}
--- Intern_roadmap/Xác định enum, trạng thái Task_w1/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.example.BKENDelitesport.Service;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
